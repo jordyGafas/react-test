b3f6fb14619ae50956b650d861783b940a3ae70c
Jordy <jordy@gafas.be>
2019-11-12 13:10:49 +0100

    Update webpack.config.js
    test

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const meteorExternals = require("webpack-meteor-externals");
 
 const clientConfig = {
-  entry: ["./client/main.jsx", "babel-polyfill"],
+  entry: "./client/main.jsx",
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules\/(?!(base|base64-js|meteor-node-stubs|swiper)\/).*/,
+        exclude: /node_modules/,
         use: ["babel-loader"]
       },
       {
@@ -35,19 +35,6 @@ const clientConfig = {
 const serverConfig = {
   entry: ["./server/main.js"],
   target: "node",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules\/(?!(base|base64-js|meteor-node-stubs|swiper)\/).*/,
-        use: ["babel-loader"]
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
   devServer: {
     hot: true
   },
